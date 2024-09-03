9d2598d3eebed7d4ff4857496e2ec538b6b6c095
Gogani <93490682+GiovanniKarra@users.noreply.github.com>
2024-09-04 00:19:17 +0200

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Google Dark (Gogh)'
+config.font = wezterm.font 'Monospace'
+config.font = wezterm.font 'Fira Mono'
+config.font_size = 14
+
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
+config.keys = {
+  {
+    key = 'Backspace',
+    mods = 'CTRL',
+    action = wezterm.action.SendKey {key = 'w', mods = 'CTRL'},
+  },
+}
+-- and finally, return the configuration to wezterm
+return config
